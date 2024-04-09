0a589e4a147d6a2d9d5be50a82ffcd0912aa83fb
Renzo Minelli <renminelli@gmail.com>
2024-04-08 23:18:18 -0300

    inicio ej4

---
+++ p3ej4.pl
@@ -0,0 +1,73 @@
+% Ejercicio 4
+% Escriba un programa Prolog que resuelva el problema de las novias celosas. Este
+% problema consiste en tres parejas que deben cruzar un río de una orilla a otra en un
+% bote cuya capacidad es de dos personas. La solución consiste en encontrar una
+% secuencia de viajes de manera tal que en ningún momento un hombre se encuentre
+% en presencia de otras mujeres sin su novia. Tenga en cuenta que, en cada cruce,
+% cualquiera de los seis puede manejar el bote si se encuentra en su orilla.
+
+% estado(Izq,Barco,Der,PosBarco).
+
+mujer(m_1).
+mujer(m_2).
+mujer(m_3).
+
+hombre(h_1).
+hombre(h_2).
+hombre(h_3).
+
+estado_invalido(estado(_,[m_1,h_2],_,_)).
+estado_invalido(estado(_,[m_1,h_3],_,_)).
+estado_invalido(estado(_,[m_2,h_1],_,_)).
+estado_invalido(estado(_,[m_2,h_3],_,_)).
+estado_invalido(estado(_,[m_3,h_1],_,_)).
+estado_invalido(estado(_,[m_3,h_2],_,_)).
+estado_invalido(estado([h_1|R],_,_,_)) :- mujer(M), member(M,R), \+member(m_1,R).
+estado_invalido(estado([h_2|R],_,_,_)) :- mujer(M), member(M,R), \+member(m_2,R).
+estado_invalido(estado([h_3|R],_,_,_)) :- mujer(M), member(M,R), \+member(m_3,R).
+estado_invalido(estado(_,_,[h_1|R],_)) :- mujer(M), member(M,R), \+member(m_1,R).
+estado_invalido(estado(_,_,[h_2|R],_)) :- mujer(M), member(M,R), \+member(m_2,R).
+estado_invalido(estado(_,_,[h_3|R],_)) :- mujer(M), member(M,R), \+member(m_3,R).
+
+subir_barco(estado(Izq,Barco,Der,barco_izq), estado(IzqResto,NBarco,Der,barco_izq)):-
+    length(Barco,CantTripulantes),
+    CantTripulantes < 2,
+    select(P,Izq,IzqResto),
+    append([P],Barco,NBarco),
+    \+estado_invalido(estado(IzqResto,NBarco,Der,barco_izq)).
+
+subir_barco(estado(Izq,Barco,Der,barco_der), estado(Izq,NBarco,DerResto,barco_der)):-
+    length(Barco,CantTripulantes),
+    CantTripulantes < 2,
+    select(P,Der,DerResto),
+    append([P],Barco,NBarco),
+    \+estado_invalido(estado(Izq,NBarco,DerResto,barco_der)).
+
+bajar_barco(estado(Izq,Barco,Der,barco_izq), estado(NIzq,NBarco,Der,barco_izq)):-
+    length(Barco,CantTripulantes),
+    CantTripulantes > 0,
+    select(P,Barco,NBarco),
+    append([P],Izq,NIzq),
+    \+estado_invalido(estado(NIzq,NBarco,Der,barco_izq)).
+    
+bajar_barco(estado(Izq,Barco,Der,barco_der), estado(Izq,NBarco,NDer,barco_der)):-
+    length(Barco,CantTripulantes),
+    CantTripulantes > 0,
+    select(P,Barco,NBarco),
+    append([P],Der,NDer),
+    \+estado_invalido(estado(Izq,NBarco,NDer,barco_der)).
+
+cambiar_estado(estado(Izq,Barco,Der,barco_izq), estado(IzqN,BarcoN,Der,barco_izq)) :-
+    bajar_barco(estado(Izq,Barco,Der,barco_izq), estado(IzqN,BarcoN,Der,barco_izq)).
+
+cambiar_estado(estado(Izq,Barco,Der,barco_izq), estado(IzqN,BarcoN,Der,barco_izq)) :-
+    subir_barco(estado(Izq,Barco,Der,barco_izq), estado(IzqN,BarcoN,Der,barco_izq)).
+
+cambiar_estado(estado(Izq,Barco,Der,barco_der), estado(Izq,BarcoN,DerN,barco_der)) :-
+    bajar_barco(estado(Izq,Barco,Der,barco_der), estado(Izq,BarcoN,DerN,barco_der)).
+
+cambiar_estado(estado(Izq,Barco,Der,barco_der), estado(Izq,BarcoN,DerN,barco_der)) :-
+    subir_barco(estado(Izq,Barco,Der,barco_der), estado(Izq,BarcoN,DerN,barco_der)).
+
+cambiar_estado(estado(Izq,Barco,Der,barco_izq),estado(Izq,Barco,Der,barco_der)).
+cambiar_estado(estado(Izq,Barco,Der,barco_der),estado(Izq,Barco,Der,barco_izq)).
