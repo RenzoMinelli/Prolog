35a2621fd651240bf607a820c0e7dbfc62df7d19
Renzo Minelli <renminelli@gmail.com>
2024-04-26 20:51:26 -0300

    fix de lab

--- lab1.pl
+++ lab1.pl
@@ -28,7 +28,7 @@ contenida([X|XT], L):-
 % Ej.: permutacion([a,b,c],[c,a,b]).
 
 permutacion([], []).
-permutacion([N|L], R) :- permutacion(L,I), elegir(N,R,I).
+permutacion(R,[N|L]) :- elegir(N,R,I), permutacion(I,L).
 
 % suma(+L,?S) ← S es la suma de los elementos de la lista L.
 % Ej: suma([1,2,3,4],10).
@@ -72,22 +72,26 @@ columna([[F|RestoFila]|MT], [F|CT], [RestoFila|RT]):-
 transpuesta([[]|_], []).
 transpuesta(M, [C|T]) :- columna(M, C, R), transpuesta(R, T).
 
-% bloques(+M,+K,?B) ← M es una matriz que representa un sudoku de orden K, B es
-% su lista de bloques, donde cada bloque es una lista de K² números obtenidos de
-% M. Notar que cada bloque es lista simple, no lista de listas. La cantidad de bloques
-% a obtener será también K². Por ejemplo (ver también Fig. 2):
-% ?- bloques([[2,1,3,4], [4,3,2,1], [1,2,4,3], [3,4,1,2]], 2, B).
-% B = [[2,1,4,3], [3,4,2,1], [1,2,3,4], [4,3,1,2]]
+% achatar(+L1,?L2) ← L1 es una lista de listas, L2 es la lista resultante de la concatenación de las listas de L1
+% Ej.: achatar([[1,2,3],[4,5,[6]]], [1,2,3,4,5,[6]]).
 
 achatar([],[]).
 achatar([L|Ls],R) :-
     achatar(Ls,LsAchatado),
     append(L,LsAchatado,R).
 
+% primeros_k_de_filas(+M,+K,?B,?R) ← M es una matriz con al menos K columnas, B son las primeras K columnas y 
+% R es el resultado de retirar esas primeras K columnas de M.
+% Ej.: primeros_k_de_filas([[1,2],[4,5]],1,[[1],[4]],[[2],[5]]).
+
 primeros_k_de_filas([],_,[],[]).
 primeros_k_de_filas([F|M],K,[KElems|Fs],[RestoFila|RestoFilasM]) :-
     tomar_n(F,K,KElems,RestoFila),
     primeros_k_de_filas(M,K,Fs,RestoFilasM).
+
+% bloques_en_fila(+M,+K,?L) ← M es una matriz con K filas y un múltiplo de K columnas, L es una lista de listas 
+% donde cada una de ellas contiene los elementos correspondientes a los bloques en M.
+% Ej.: bloques_en_fila([[1,2,3,4],[1,2,3,4]],2,[[1,2,1,2], [3,4,3,4]]).
 
 bloques_en_fila([[]|_],_,[]).
 bloques_en_fila(M,K,[L1|RestoBloques]) :-
@@ -95,12 +99,29 @@ bloques_en_fila(M,K,[L1|RestoBloques]) :-
     achatar(B1,L1),
     bloques_en_fila(RestoM,K,RestoBloques).
 
+% bloques(+M,+K,?B) ← M es una matriz que representa un sudoku de orden K, B es
+% su lista de bloques, donde cada bloque es una lista de K² números obtenidos de
+% M. Notar que cada bloque es lista simple, no lista de listas. La cantidad de bloques
+% a obtener será también K². 
+% ?- bloques([[2,1,3,4], [4,3,2,1], [1,2,4,3], [3,4,1,2]], 2, B).
+% B = [[2,1,4,3], [3,4,2,1], [1,2,3,4], [4,3,1,2]]
+
 bloques([],_,[]).
 bloques(M,K,Bloques) :-
     tomar_n(M,K,KPrimerasFilas,RestoFilas),
     bloques_en_fila(KPrimerasFilas,K,BloquesFila),
     bloques(RestoFilas,K,BloquesResto),
-    append(BloquesFila,BloquesResto,Bloques). % ver si se puede mergear de otra forma
+    append(BloquesFila,BloquesResto,Bloques).
+
+% chequear_permutacion(+L,?Ls) ← L es una lista, Ls es una lista de listas donde cada lista es una permutacion de L.
+% Ej.: chequear_permutacion([1,2],[[1,2],[2,1]]).
+
+chequear_permutacion(ListaK,[L]) :- 
+    permutacion(ListaK,L).
+
+chequear_permutacion(ListaK,[L|Ls]) :-
+    permutacion(ListaK,L),
+    chequear_permutacion(ListaK,Ls).
 
 % sudoku(+M,+K) ← M es una matriz que representa un sudoku de orden K, el
 % predicado es verdadero si M es un sudoku correcto resuelto. Notar que podrá
@@ -109,19 +130,12 @@ bloques(M,K,Bloques) :-
 % ?- M=[[1,_,_,2],[_,2,_,_],[_,_,4,1],[_,_,_,3]],sudoku(M,2).
 % M = [[1,4,3,2],[3,2,1,4],[2,3,4,1],[4,1,2,3]]
 
-chequear_permutacion([L],ListaK) :- 
-    permutacion(ListaK,L).
-
-chequear_permutacion([L|Ls],ListaK) :-
-    permutacion(ListaK,L),
-    chequear_permutacion(Ls,ListaK).
-
 sudoku(M,K):-
     K>0,
     K2 is K**2,
     rango(K2,ListaK),
-    chequear_permutacion(M,ListaK),
+    chequear_permutacion(ListaK,M),
     transpuesta(M,MT),
-    chequear_permutacion(MT,ListaK),
+    chequear_permutacion(ListaK,MT),
     bloques(M,K,B),
-    chequear_permutacion(B,ListaK).
+    chequear_permutacion(ListaK,B).
