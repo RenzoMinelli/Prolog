a6f97e04735b1ee83b40c92c498de04352837fa9
Renzo Minelli <renminelli@gmail.com>
2024-05-17 20:59:24 -0300

    Create p9ej6.pl

---
+++ p9ej6.pl
@@ -0,0 +1,36 @@
+o --> gn, gv.
+
+gn --> sust.
+gn --> art, sust.
+gn --> sust, pred, sust.
+gn --> art, sust, pred, sust.
+gn --> sust, pred, art, sust.
+gn --> art, sust, pred, art, sust.
+
+gv --> verb. 
+gv --> verb, gn. 
+gv --> verb, pred, gn. 
+
+art --> [los].
+art --> [la].
+art --> [las].
+art --> [el].
+
+sust --> [osos_polares].
+sust --> [peces].
+sust --> [antartida].
+sust --> [investigador].
+sust --> [investigadores].
+sust --> [comportamiento].
+sust --> [aves].
+sust --> [alimento].
+
+pred --> [de].
+pred --> [a].
+pred --> [en].
+
+verb --> [comen].
+verb --> [viven].
+verb --> [da].
+verb --> [estudian].
+
