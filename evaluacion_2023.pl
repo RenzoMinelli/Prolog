92746ca6877fa42851bf7a4141783435a0823948
Renzo Minelli <renminelli@gmail.com>
2024-06-29 02:18:32 -0300

    Update evaluacion_2023.pl

--- evaluacion_2023.pl
+++ evaluacion_2023.pl
@@ -65,3 +65,46 @@ problema(Camino):-
     T < 15.
     
     
+1/6::dado(1,1);1/6::dado(1,2);1/6::dado(1,3);1/6::dado(1,4);1/6::dado(1,5);1/6::dado(1,6).
+1/6::dado(2,1);1/6::dado(2,2);1/6::dado(2,3);1/6::dado(2,4);1/6::dado(2,5);1/6::dado(2,6).
+1/6::dado(3,1);1/6::dado(3,2);1/6::dado(3,3);1/6::dado(3,4);1/6::dado(3,5);1/6::dado(3,6).
+
+escalera:-
+    dado(X,1), dado(Y,2), dado(Z,3),
+    X \= Y, Y\=Z.
+
+escalera:-
+    dado(X,2), dado(Y,3), dado(Z,4),
+    X \= Y, Y\=Z.
+
+escalera:-
+    dado(X,3), dado(Y,4), dado(Z,5),
+    X \= Y, Y\=Z.
+
+escalera:-
+    dado(X,4), dado(Y,5), dado(Z,6),
+    X \= Y, Y\=Z.
+
+todos_iguales :-
+    dado(1,X), dado(2,X), dado(3,Z).
+query(escalera).
+query(todos_iguales).
+
+% dado(1).
+% dado(2).
+% dado(3).
+
+% 1/6::elegir(X,1) ; 1/6::elegir(X,2) ; 1/6::elegir(X,3) ; 1/6::elegir(X,4) ; 1/6::elegir(X,5) ; 1/6::elegir(X,6).
+
+% dado(X,Y) :- dado(X),elegir(X,Y).
+% todos_iguales2 :- dado(1,Y), dado(2,Y), dado(3,Y).
+
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),Y is X+1, Z is Y+1. % 1 2 3
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),Y is X+2, Z is X+1. % 1 3 2
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),X is Y+1, Z is Y+2. % 2 1 3
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),X is Z+2, Y is Z+1. % 2 3 1
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),X is Y+2, Z is Y+1. % 3 1 2
+escalera2 :- dado(1,X),dado(2,Y),dado(3,Z),X is Z+2, Y is Z+1. % 3 2 1
+
+query(escalera2).
+% query(todos_iguales2).
