0b9b713fef229dad8a21202f0e30433bf7669b3d
Renzo Minelli <renminelli@gmail.com>
2024-07-05 17:11:26 -0300

    Update ev_2020_v2_ej1.pl

--- pruebas/ev_2020_v2_ej1.pl
+++ pruebas/ev_2020_v2_ej1.pl
@@ -1,12 +1,12 @@
 adyacente(T,I1,J1,I1,J2):-
     member(celda(I1,J2,_),T),
     Dif is J1-J2,
-    abs(Dif,1).
+    1 is abs(Dif).
 
 adyacente(T,I1,J1,I2,J1):-
     member(celda(I2,J1,_),T),
     Dif is I1-I2,
-    abs(Dif,1).
+    1 is abs(Dif).
 
 valor_celda([celda(I,J,V)|_],I,J,V) :- !.
 valor_celda([_|Ts],I,J,V):-
@@ -29,28 +29,26 @@ ciclo(T,I1,J1,I2,J2,Ciclo):-
     adyacente(T,I1,J1,I2,J2),
     viaje(T,I1,J1,I2,J2,Ciclo).
 
-viajes_para_celdas(_,[],[]).
+viajes_para_celdas(_, [], []).
 
-viajes_para_celdas(T,[celda(I,J,_)|Celdas],Salida):-
-    writeln('(I,J)'),
-    writeln((I,J)),
-    bagof(Camino,viaje(T,I,J,_,_,Camino),CaminosCelda),
-    writeln('CAminosCelda:'),
-    writeln(CaminosCelda),
-    viajes_para_celdas(T,Celdas,CaminosResto),
-    append(CaminosCelda,CaminosResto, Salida).
+viajes_para_celdas(T, [celda(I,J,_)|Celdas], Salida):-
+    bagof(Camino, viaje(T,I,J,I2,J2,Camino), CaminosCelda),
+    viajes_para_celdas(T, Celdas, CaminosResto),
+    append(CaminosCelda, CaminosResto, Salida).
 
-camino_mas_largo([],LargoAcum,LargoAcum).
+viajes_para_celdas(T, [_|Celdas], Salida):-
+    viajes_para_celdas(T, Celdas, Salida).
 
-camino_mas_largo([C|Cs],LargoAcum,LargoMejor):-
-    length(C,Largo),
+camino_mas_largo([], LargoAcum, LargoAcum).
+
+camino_mas_largo([C|Cs], LargoAcum, LargoMejor):-
+    length(C, Largo),
     Largo > LargoAcum, !,
-    camino_mas_largo(Cs,Largo,LargoMejor).
+    camino_mas_largo(Cs, Largo, LargoMejor).
 
-camino_mas_largo([_|Cs],LargoAcum,LargoMejor):-
-    camino_mas_largo(Cs,LargoAcum,LargoMejor).
+camino_mas_largo([_|Cs], LargoAcum, LargoMejor):-
+    camino_mas_largo(Cs, LargoAcum, LargoMejor).
 
-maximo_largo_de_viaje(T,N):-
-    viajes_para_celdas(T,T,Viajes),
-    writeln(Viajes),
-    camino_mas_largo(Viajes,0,N).
+maximo_largo_de_viaje(T, N):-
+    viajes_para_celdas(T, T, Viajes),
+    camino_mas_largo(Viajes, 0, N).
